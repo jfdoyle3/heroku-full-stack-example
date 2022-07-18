2bcb575577722e5f1718b2cdc0ba365ed6d989ae
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-07-18 16:28:18 -0400

    simple app

---
+++ src/main/java/com/example/herokufullstackexample/HerokuFullStackExampleApplication.java
@@ -0,0 +1,25 @@
+package com.example.herokufullstackexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@SpringBootApplication
+public class HerokuFullStackExampleApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuFullStackExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokufullstackexample/HerokuFullStackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokufullstackexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuFullStackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
